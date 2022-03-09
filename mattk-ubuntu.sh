82f0487fdfd549300ebab5ea82a028ed50dbbfd6
SoniX213 <30598689+SoniX213@users.noreply.github.com>
2022-03-08 18:49:49 -0600

    Update mattk-ubuntu.sh

--- mattk-ubuntu.sh
+++ mattk-ubuntu.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y nvidia-dkms-470 openjdk-8-jre-headless openjdk-11-jre-headless openjdk-16-jre-headless nut-client samba nut git make npm nfs-common htop net-tools lm-sensors apt-transport-https software-properties-common wget gdebi-core libgl1-mesa-glx:i386 ubuntu-desktop-minimal && sudo apt upgrade -y
+sudo apt install -y openjdk-8-jre-headless openjdk-11-jre-headless openjdk-16-jre-headless nut-client samba nut git make npm nfs-common htop net-tools lm-sensors apt-transport-https software-properties-common wget gdebi-core libgl1-mesa-glx:i386 ubuntu-desktop-minimal && sudo apt upgrade -y
 echo " Packages & System Upgraded "
 
 wget -O ~/steam.deb http://media.steampowered.com/client/installer/steam.deb
